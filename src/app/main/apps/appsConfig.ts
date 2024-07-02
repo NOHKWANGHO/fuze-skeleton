7952754ad53ebf9a7da3cc02ae7925524cc1e807
NOHKWANGHO <jpkh6169@gmail.com>
2024-07-02 15:02:21 +0900

    first commit

---
+++ src/app/main/apps/appsConfig.ts
@@ -0,0 +1,10 @@
+import { FuseRouteConfigsType } from "@fuse/utils/FuseUtils";
+
+import ContactsAppConfig from "./contacts/ContactsAppConfig";
+
+/**
+ * The list of application configurations.
+ */
+const appsConfigs: FuseRouteConfigsType = [ContactsAppConfig];
+
+export default appsConfigs;
